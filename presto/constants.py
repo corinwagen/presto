151f053af73fbb77cce2a63e66e5ae72d8cce860
Eugene Kwan <ekwan16@gmail.com>
2020-04-24 15:21:56 -0400

    forces were negative forces goshdarnit

--- presto/constants.py
+++ presto/constants.py
@@ -10,7 +10,7 @@ Base units for *presto*:
 Forces from most computational chemistry software come in Hartrees/Bohr.
 
 x kcal      4184 J      1 kg m^2 s^-2     10^20 Å^2         1 s^2       1000 g      1 amu
--------  * -------- *  --------------- * ----------- * ------------- * -------- *  --------- = 0.0004184 * x amu Å fs^-2
+-------  * -------- *  --------------- * ----------- * ------------- * -------- *  --------- = 0.0004184 * x amu Å fs^-2 * mol * A / K
 mol * Å     1 kcal           1 J             m^2        10^30 fs^2      1 kg        1 g/mol
 
 """
@@ -26,6 +26,5 @@ AMU_A2_FS2_PER_KCAL_MOL_ANGSTROM = 0.0004184
 AMU_A2_FS2_PER_HARTREE_BOHR = 0.4961455 # AMU_A2_FS2_PER_KCAL_MOL_ANGSTROM * KCAL_MOL_ANGSTROMS_PER_HARTREE_BOHR
 
 BOLTZMANN_CONSTANT = 0.0019872041 * 0.0004184
-
 
 
