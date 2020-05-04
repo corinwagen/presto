fb1a02a54dacf1ed9a0f2087117841946a880916
Corin Wagen <corin.wagen@gmail.com>
2020-05-04 14:56:01 -0400

    working on running rxns

---
+++ test/run_trajectory.py
@@ -0,0 +1,62 @@
+import unittest, cctk
+import numpy as np
+from datetime import datetime
+
+import sys
+sys.path.append('../presto')
+
+import presto
+
+def boring_scheduler(time):
+    return 298
+
+start = cctk.XYZFile.read_file("test/static/solvated_tetrahedral_TS.xyz").molecule
+traj = presto.trajectory.EquilibrationTrajectory(
+    timestep=0.5,
+    atomic_numbers=start.atomic_numbers,
+    high_atoms=np.array([]),
+    inactive_atoms=np.array([1,7,8]),
+    calculator=presto.calculators.XTBCalculator(),
+    integrator=presto.integrators.VelocityVerletIntegrator(),
+    bath_scheduler=boring_scheduler,
+    stop_time = 200,
+)
+
+print(datetime.now())
+traj.run(checkpoint_filename="test/static/tet_equil.chk", checkpoint_interval=10, positions=start.geometry)
+print(datetime.now())
+print(f"{len(traj.frames)} frames run")
+
+temps = [f.temperature() for f in traj.frames][10:]
+energies = [f.energy for f in traj.frames][10:-1]
+
+print(f"TEMPERATURE:\t\t{np.mean(temps):.2f} (± {np.std(temps):.2f})")
+print(f"ENERGY:\t\t\t{np.mean(energies):.2f} (± {np.std(energies)*627.509:.2f} kcal/mol)")
+
+traj.write_movie("test/static/tet_equil.pdb")
+
+print("done equil")
+
+def termination(frame):
+    mol = frame.molecule().assign_connectivity()
+    if mol.get_bond_order(1,7):
+        return True
+    if mol.get_bond_order(1,8):
+        return True
+    return False
+
+f_run, r_run = traj.spawn(termination, 250)
+
+print("\nrunning forward trajectory")
+print(datetime.now())
+f_run.run("test/static/tet_run_f.chk", checkpoint_interval=10)
+print(datetime.now())
+print(f"{len(f_run.frames)} frames run")
+f_run.write_movie("test/static/tet_run_f.pdb")
+
+print("\nrunning reverse trajectory")
+print(datetime.now())
+r_run.run("test/static/tet_run_r.chk", checkpoint_interval=10)
+print(datetime.now())
+print(f"{len(r_run.frames)} frames run")
+r_run.write_movie("test/static/tet_run_r.pdb")
