f99d2d32d86b85100be91ee43e2ab57e63e84a4f
Eugene Kwan <ekwan16@gmail.com>
2020-05-05 16:07:18 -0400

    working on gaussian code

--- presto/calculators.py
+++ presto/calculators.py
@@ -2,9 +2,35 @@ import numpy as np
 import cctk
 import ctypes
 
+import os
+import random
+import string
+import subprocess
+
 import xtb
 
 from presto import constants
+
+###########################
+### Gaussian Parameters ###
+###########################
+
+# A-Za-z0-9
+LETTERS_AND_DIGITS = string.ascii_letters + string.digits
+
+# select a unique 8-letter string that will
+# be used to identify this particular Python process
+UNIQUE_ID = random.choice(string.ascii_letters)
+for i in range(7):
+    UNIQUE_ID += random.choice(LETTERS_AND_DIGITS)
+
+# will be incremented by one every time a calculation is run
+COUNTER = 0
+
+# this is the absolute path to the directory where the run_gaussian.sh script is
+GAUSSIAN_DIRECTORY = f"{os.getcwd()}/gaussian/"
+
+###########################
 
 class Calculator():
     """
@@ -20,12 +46,12 @@ class Calculator():
 class XTBCalculator(Calculator):
     """
     """
-    def evaluate(self, atomic_numbers, positions):
+    def evaluate(self, atomic_numbers, positions, params={}):
         """
         Args:
-            positions (cctk.OneIndexedArray):
             atomic_numbers (cctk.OneIndexedArray):
-            params (dict): custom params for calculation
+            positions (cctk.OneIndexedArray):
+            params (dict): custom params for calculation (not currently implemented)
 
         Returns:
             energy (float): in Hartree
@@ -69,7 +95,57 @@ class XTBCalculator(Calculator):
 
 class GaussianCalculator(Calculator):
 
-    def evaluate(self, molecule):
+    def evaluate(self, positions, atomic_numbers, charge=0, multiplicity=1,
+                 link0={"mem":"1GB", "nprocshared":"4"}
+                 route_card="hf 3-21g force"
+                 footer=None):
+        """
+        Gets the electronic energy and cartesian forces for the specified geometry.
+
+        Args:
+            positions (cctk.OneIndexedArray): the atomic positions in angstroms
+            atomic_numbers (cctk.OneIndexedArray): the atomic numbers (int)
+
+        Returns:
+            energy (float): in Hartree
+            forces (cctk.OneIndexedArray): in amu Å per fs**2
+        """
+        # set working directory
+        old_working_directory = os.getcwd()
+        os.chdir(GAUSSIAN_DIRECTORY)
+
+        # generate filenames
+        COUNTER += 1
+        this_unique_id = f"{UNIQUE_ID}-{COUNTER:09d}"
+        input_filename = f"presto-{this_unique_id}.gjf"
+        output_filename = f"presto-{this_unique_id}.out"
+
+        # create molecule
+        molecule = cctk.Molecule(atomic_numbers, geometry, charge=charge, multiplicity=multiplicity)
+
+	# write out job
+        cctk.GaussianFile.write_molecule_to_file(input_filename, molecule, route_card, link0, footer)
+
+        # run job
+        command = ["bash", "run_gaussian.sh", this_unique_id]
+        process = sp.Popen(command, stdout=sp.PIPE)
+        output, error = process.communicate()
+        p_status = process.wait()
+        output = output.decode("utf-8")
+        print(output)
+        print(p_status)
+
+        # get results and change units
+        file = cctk.GaussianFile.read_file(output_filename)
+        ensemble = file.ensemble
+        molecule = file.molecules[-1]
+        properties_dict = ensemble.get_properties_dict(molecule)
+        print(properties_dict)
+
+        # restore working directory
+        os.chdir(old_working_directory)
+
+        # return result
         pass
 
 class ONIOMCalculator(Calculator):
