45c00d478bdd3e8d564ef2285f6ab98477f26dcd
Corin Wagen <corin.wagen@gmail.com>
2020-05-09 14:15:37 -0400

    added gfn0-xtb support, found errors with XTB and ONIOM paralleization

--- test/test_xtb.py
+++ test/test_xtb.py
@@ -24,3 +24,11 @@ class TestXTB(unittest.TestCase):
             for entry1,entry2 in zip(row1, row2):
                 delta = abs(entry1-entry2)
                 self.assertLessEqual(delta, 0.00000001)
+
+    def test_gfn0(self):
+        hydrogen_molecule = cctk.XYZFile.read_file("test/static/H2.xyz").molecule
+        atomic_numbers = hydrogen_molecule.atomic_numbers
+        positions = hydrogen_molecule.geometry
+        xtb_calculator = calculators.XTBCalculator(charge=0, multiplicity=1, gfn=0)
+        energy, forces = xtb_calculator.evaluate(atomic_numbers, positions)
+        self.assertLessEqual(abs(energy+1.0083914049), 0.00000001)
