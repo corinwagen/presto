e4c98afa423db0e069c2ada2624d7ea403631d8d
Corin Wagen <corin.wagen@gmail.com>
2020-11-28 13:14:14 -0500

    delete extraneous tutorial csv

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="presto-md",
+    packages=["presto", "presto.solvents"],
+    package_data={"presto.solvents": ["*"],},
+    version="0.0.1",
+    author="Corin Wagen",
+    author_email="corin.wagen@gmail.com",
+    license="Apache 2.0",
+    description="explicit solvent molecular dynamics",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cwagen/presto",
+#    packages=setuptools.find_packages(),
+    install_requires=["cctk", "tqdm", "h5py", "pyyaml"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: Apache Software License",
+        "Intended Audience :: Science/Research",
+    ],
+    python_requires='>=3.6',
+)
