e807c211cd3839c5cd05eaa3eafd1191b78878ea
Corin Wagen <corin.wagen@gmail.com>
2020-11-29 16:56:55 -0500

    improve analyze doc

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -2,8 +2,8 @@ import argparse, math, sys, os, subprocess, glob, re, shutil, logging, presto
 import numpy as np
 from asciichartpy import plot
 
-parser = argparse.ArgumentParser(prog="analyze.py", description="Analyze a single presto job -- prints temperature and energy as a function of time, and can write movie.")
-parser.add_argument("--cutoff", "-c", default=0, type=int, help="index of first frame to analyze, defaults to 0 (analyzing all frames)")
+parser = argparse.ArgumentParser(prog="analyze.py", description="Analyze a single presto job -- prints temperature and energy as a function of time, and can write a movie.")
+parser.add_argument("--cutoff", "-c", default=0, type=int, help="index of first frame to analyze, defaults to 0 (i.e. analyzing all frames)")
 parser.add_argument("--movie", "-m", default=False, help="which atoms to include in movie, either 'high' or 'all'. if blank, no movie will be written.")
 parser.add_argument("config_filename", type=str, help="path to config file (usually ends in .yaml)")
 parser.add_argument("checkpoint_filename", type=str, help="path to checkpoint file (usually ends in .chk)")
