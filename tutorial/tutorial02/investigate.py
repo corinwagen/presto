69c1e06d96efb06e140661034afb4cc4df20ba3e
Corin Wagen <corin.wagen@gmail.com>
2021-01-04 07:34:36 -0500

    save tutorial progress

--- tutorial/tutorial02/investigate.py
+++ tutorial/tutorial02/investigate.py
@@ -1,7 +1,7 @@
 import sys, presto, logging, re
 
-traj = presto.config.build("preequil.yaml", "preequil.chk", geometry="solvated.xyz")
+traj = presto.config.build("preequil.yaml", "preequil.chk")
 print(traj.active_atoms)
 
-for idx, frame in enumerate(traj.frames[::10]):
+for idx, frame in enumerate(traj.frames[::100]):
     print(f"{idx}\t{frame.positions[1]}\t{frame.positions[11]}")
