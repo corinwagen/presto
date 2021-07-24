015617afca3cf109e7a58ceaeab174d72bc98002
msh-yi <46651385+msh-yi@users.noreply.github.com>
2021-07-24 03:24:27 -0400

    fix demuxing script

--- scripts/remd_demux.py
+++ scripts/remd_demux.py
@@ -26,6 +26,8 @@ if __name__ == "__main__":
         prog="remd_demux.py", description="Given a REMD run, reconstructs the continuous trajectories of each replica and outputs trajectory checkfiles and the temperature evolution for each replica. Run $ python remd_demux.py --help for options.")
     parser.add_argument("--checkpoint_filename", "-c", type=str, default="remd.chk",
                         help="path to checkpoint file (usually ends in .chk)")
+    parser.add_argument("--frames", "-t", type=int, default=-1,
+                        help="length of run the beginning (in fs) to demux and analyze")
 
     args = vars(parser.parse_args(sys.argv[1:]))
     chkfile = args["checkpoint_filename"]
@@ -38,12 +40,19 @@ if __name__ == "__main__":
 
     trajs = remd.trajectories
     swap_int = int(remd.swap_interval)
-    end_time = int(remd.current_idx * remd.swap_interval)
-    assert trajs[0].last_time_run() == end_time, "End time recorded in trajectory checkpoint file does not agree with end time recorded in REMD checkpoint file."
+    run_time = int(remd.current_idx * remd.swap_interval) # frames that have been run, in fs
+
+    assert trajs[0].last_time_run() == run_time, "End time recorded in trajectory checkpoint file does not agree with end time recorded in REMD checkpoint file."
+
+    if args['frames'] == -1: # all frames
+        end_time = run_time
+    else:
+        end_time = args['frames']
+        assert end_time <= run_time, f"Only {run_time} fs have been run, but {end_time} fs requested"
+        assert end_time % swap_int == 0, f"Requested length must be multiple of swap interval ({swap_int} fs)."
 
     # list of lists of temp history
-    traj_hists = np.zeros(
-        (len(trajs), int(end_time/swap_int) + 1), dtype=int)
+    traj_hists = np.zeros((len(trajs), int(run_time/swap_int) + 1), dtype=int)
     # add 1 to shape[1] if we don't want to ignore the last swap
 
     for i, hist in enumerate(traj_hists):
@@ -55,37 +64,50 @@ if __name__ == "__main__":
     for i, swap in enumerate(remd.swaps):
         while int(swap["time"]) != curr_time:
             # finished swapping or no swaps for this time, write final arrangement for curr_time
+            print(f'at {curr_time} fs, the arrangement is {curr_arrangement}')
             traj_hists[:, int(curr_time/swap_int)] = curr_arrangement
             curr_time += swap_int
         exchange(curr_arrangement, swap["i"], swap["j"])
     
     #account for any final intervals without swaps
-    while curr_time != end_time:
+    while curr_time <= run_time:
+        print(f'at {curr_time} fs, the arrangement is {curr_arrangement}')
         traj_hists[:, int(curr_time/swap_int)] = curr_arrangement
-        curr_time += swap_int
+        
+        if curr_time != run_time:
+            curr_time += swap_int
+        else:
+            break
     
-    print(f"{end_time} fs have been run, and the swap interval is {swap_int} fs.")
+    logger.info(f"{run_time} fs have been run, {end_time} fs will be demuxed, and the swap interval is {swap_int} fs.")
     assert curr_time/swap_int == traj_hists.shape[1] - 1  # last column
+
+    demux_hists = np.zeros((len(trajs), int(run_time/swap_int) + 1), dtype=int) # same shape as traj_hists
     
+    # actually demuxing
+    for i,j in np.ndindex(traj_hists.shape):
+        demux_hists[traj_hists[i,j], j] = i    
+
     demux_trajs = copy.deepcopy(trajs)
     for traj in demux_trajs:
         traj.frames = []  # empty frames, to be filled in demuxed order
 
-    n_frames = (traj_hists.shape[1] - 1) * swap_int / int(traj.save_interval) + 1 
+    n_frames = int(end_time / int(trajs[0].timestep * trajs[0].save_interval)) + 1 # make sure traj.timestep is needed
     # include zeroth frame
 
     print("loading trajectories...")
     for traj in tqdm(trajs):
-        traj.load_from_checkpoint()  # loads every frame
+        traj.load_from_checkpoint(frames=slice(0, n_frames, 1))
         assert len(
             traj.frames) == n_frames, "expected number of frames should be equal to number of frames in trajectory"
 
     #hist = np.repeat(hist, swap_int / traj.save_interval)
 
+
     print("demuxing trajectories...")
-    for i, hist in tqdm(enumerate(traj_hists)):  # each replica
+    for i, hist in tqdm(enumerate(demux_hists)):  # each trajectory
         for j in np.arange(n_frames, dtype=int): # each timestep
-            demux_trajs[i].frames.append(trajs[traj_hists[i, int(j/swap_int)]].frames[j])
+            demux_trajs[i].frames.append(trajs[demux_hists[i, int(j/swap_int)]].frames[j])
 
     print("writing demuxed trajectories to PDB and .chk files:")
     for i, traj in tqdm(enumerate(demux_trajs)):
@@ -97,12 +119,13 @@ if __name__ == "__main__":
         traj.save()
 
     times = np.arange(0, end_time + 1, swap_int)
-    assert traj_hists.shape[1] == times.size, "length of time array should be the same as number of swaps"
-
+    
     print("writing temperature evolutions to CSV files...")
-    for i, hist in tqdm(enumerate(traj_hists)):
-        time_hist = np.array([times, hist]).astype(int)
+    for i, hist in tqdm(enumerate(demux_hists)):
+        #time_hist = np.array([times, hist[:n_frames]]).astype(int)
+        time_hist = np.stack((times, hist[:len(times)])).astype(int)
+
         csv_filename = f"replica{i}.csv"
-        np.savetxt(csv_filename, time_hist.transpose(), delimiter=',', header='time(fs), temperature index')
+        np.savetxt(csv_filename, time_hist.transpose(), delimiter=',', header='tim  e(fs), temperature index')
         logger.info(f"Wrote temperature evolution to {csv_filename}")
 
