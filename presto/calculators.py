20618a8c9642e7c430be2de6c5abb904d1ef14e0
Eugene Kwan <ekwan16@gmail.com>
2021-08-05 23:56:15 -0400

    bugfix 2

--- presto/calculators.py
+++ presto/calculators.py
@@ -320,7 +320,7 @@ class GaussianCalculator(Calculator):
                 break
 
         # if Gaussian failed after all these attempts, die
-        if energy is not None or forces is not None:
+        if energy is None or forces is None:
             print(f"g16 failed after {max_retries} attempts")
             sys.exit(1)
 
