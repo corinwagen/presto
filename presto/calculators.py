e4078fb541f6bff247aa2402589411aa182cf04e
Eugene Kwan <ekwan16@gmail.com>
2021-08-06 17:49:16 -0400

    debug xtb 2

--- presto/calculators.py
+++ presto/calculators.py
@@ -147,7 +147,8 @@ class XTBCalculator(Calculator):
             # if overall charges are available, copy them into the xtb temp folder
             # overall_charges should be a single-column text file containing charges in atomic number order
             # enabled only for gfnff
-            if self.topology and os.path.isfile(f"{old_working_directory}/overall_charges"):
+            if self.gfn and os.path.isfile(f"{old_working_directory}/overall_charges"):
+                print("copied")
                 shutil.copyfile(f"{old_working_directory}/overall_charges", f"{tmpdir}/charges")
 
             # run xtb
