302b1198361be9342bb123cbfd9c25818fd305bf
Eugene Kwan <ekwan16@gmail.com>
2021-08-11 10:10:46 -0400

    allowing rewinding by one frame

--- presto/controller.py
+++ presto/controller.py
@@ -71,13 +71,13 @@ class Controller():
                     logger.info(f"there are currently {trajectory_length} frames")
                     new_frame_index = trajectory_length - backwards_stride
                     logger.info(f"trying to set frame index to {new_frame_index}")
-                    if new_frame_index <= 0:
+                    if new_frame_index < 0:
                         # if we have very few frames, go to a backwards_stride of 1
                         new_frame_index = trajectory_length - 1
                         logger.info(f"that's no good, trying to set frame index to {new_frame_index}")
-                    if new_frame_index <= 0:
-                        logger.info("can't find a reasonable frame index to rewind to")
-                        raise ValueError("tried to rewind, but there aren't enough frames to go backwards by")
+                    #if new_frame_index <= 0:
+                    #    logger.info("can't find a reasonable frame index to rewind to")
+                    #    raise ValueError("tried to rewind, but there aren't enough frames to go backwards by")
 
                     # snip off some frames and try again
                     n_removed_frames = trajectory_length - new_frame_index
